5fa3554fedb58143d5be83170796eeaf37f8d4c1
Marcin Er <mar.rudzki@gmail.com>
2026-04-09 06:50:29 +0200

    Refactor tier list structure and enhance drag-and-drop functionality

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let draggedItem;
 
 document.querySelectorAll('.item').forEach(setupItem);
+document.querySelectorAll('.drop-zone').forEach(setupDropzone);
 
 function setupItem(item) {
-  item.addEventListener('dragstart', onDragItem);
+  item.addEventListener('dragstart', onDrag);
 }
 
-function onDragItem(event) {
+function setupDropzone(dropzone) {
+  dropzone.addEventListener('dragover', function (event) {
+    event.preventDefault();
+  });
+  dropzone.addEventListener('drop', onDrop);
+}
+
+function onDrag(event) {
   draggedItem = event.target;
   console.log(draggedItem.getAttribute('aria-label'));
+}
+
+function onDrop() {
+  if (this !== draggedItem.parentNode) {
+    this.appendChild(draggedItem);
+  }
 }
